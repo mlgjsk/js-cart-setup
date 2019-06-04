ea0bf9adec5f1d8f4593f23177c3078e6fe44150
mlgjsk <buba_malogajski@outlook.com>
2019-06-04 11:17:03 +0200

    Show chart

---
+++ js/app.js
@@ -0,0 +1,23 @@
+//show app
+
+(function() {
+	const cartInfo = document.getElementById('card-info');
+	const cart = document.getElementById('cart');
+
+	cartInfo.addEventListener('click', function() {
+		cart.classList.toggle('show-cart');
+	});
+})();
+
+//add items to the cart
+(function() {
+	const cartBtn = document.querySelectorAll('.store-item-icon');
+	cartBtn.forEach(function(btn) {
+		btn.addEventListener('click', function(event) {
+			//console.log(event.target);
+
+			if (event.target.parentElement.classList.contains('store-item-icon')) {
+			}
+		});
+	});
+});
